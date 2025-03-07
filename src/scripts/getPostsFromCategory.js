3c6bdf00d76c58e540f0147b2a9177260f216d62
Igorchpolak <igorchpolak@proton.me>
2025-03-07 10:53:17 +0100

    working category routing

---
+++ src/scripts/getPostsFromCategory.js
@@ -0,0 +1,30 @@
+function getImageUrlById(id, wpMediaJson) {
+
+    const result = wpMediaJson.find((inputMedia) => {
+      if (id === inputMedia.post) {
+          return true;
+      }
+  });
+  return result ? result.source_url : null;
+  }
+
+export async function getPostsArrayFromCategory(categoryId, Wordpresslink) {
+  const posts = await fetch(`${Wordpresslink}/wp-json/wp/v2/posts?categories=${categoryId}`);
+  const postsJson = await posts.json();
+
+  const media = await fetch(`${Wordpresslink}/wp-json/wp/v2/media`);
+  const mediaJson = await media.json();
+
+  const result = postsJson.map((neededPostData) => {
+    return {
+      slug: neededPostData.slug,
+      photoUrl: getImageUrlById(neededPostData.id, mediaJson),
+      title: neededPostData.title.rendered,
+      content: neededPostData.content.rendered,
+    };
+  });
+
+  return result;
+}
+
+console.log(await getPostsArrayFromCategory(2,'http://whellworks.local'));
