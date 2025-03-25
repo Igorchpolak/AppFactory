6af181d34d229267135275c5c76e0dee61842565
Igorchpolak <igorchpolak@proton.me>
2025-03-25 15:03:33 +0100

    working og images generating

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import metaTags from "astro-meta-tags";
+import { readFile } from "node:fs/promises";
+import opengraphImage from "astro-opengraph-image";
 
 export default defineConfig({
   vite: {
@@ -26,6 +28,30 @@ export default defineConfig({
     sitemap(),
     partytown(),
     metaTags(),
+    opengraphImage({
+      // what color do you want your background to be?
+      background: "#000000",
+
+      // what size do you want your images to be?
+      // 1200x630 is a good default across platforms,
+      // and 3x scale is a convenient choice.
+      width: 1200,
+      height: 630,
+      scale: 3,
+
+      // the fonts you picked before. you will have to include the particular
+      // weights and variants you want to use.
+      fonts: [
+        {
+          name: "Roboto",
+          data: await readFile(
+            "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"
+          ),
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }),
   ],
 
   env: {
